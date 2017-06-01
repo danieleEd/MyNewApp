632a3b0ba88cbec79924da085add9fa12cf6ef51
danieleEd <danyelle.edite@gmail.com>
2017-06-01 11:28:55 -0300

    ---inclusão de onCreate para opção de cadastrar---

---
+++ app/src/main/java/com/example/dani/mynewapp/DisplayMessageActivity.java
@@ -0,0 +1,17 @@
+package com.example.dani.mynewapp;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.formcadastropessoa);
+
+
+    }
+}
